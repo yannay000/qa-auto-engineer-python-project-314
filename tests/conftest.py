fa6743d4803f2a33105a6f263a5be4a884b962c3
Koval Victor <yan1992@yandex.ru>
2026-05-14 18:23:33 +0300

    return headless

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,7 @@ def driver() -> Generator[WebDriver, None, None]:
     opts.add_argument("--window-size=1366,768")
     opts.add_argument("--disable-notifications")
     opts.add_argument("--lang=ru-RU")
-    # opts.add_argument("--headless=new")
+    opts.add_argument("--headless=new")
     opts.add_argument("--no-sandbox")            # нужно в Docker
     opts.add_argument("--disable-dev-shm-usage")
     opts.add_argument("--remote-allow-origins=*") 
