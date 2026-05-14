c6daa3273de02c478f503a924f45e056a3bfde9e
Koval Victor <yan1992@yandex.ru>
2026-05-14 19:49:57 +0300

    delete comment

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,13 +8,12 @@ from selenium.webdriver.remote.webdriver import WebDriver
 
 @pytest.fixture
 def driver() -> Generator[WebDriver, None, None]:
-    # driver = webdriver.Chrome()
     opts = Options()
     opts.add_argument("--window-size=1366,768")
     opts.add_argument("--disable-notifications")
     opts.add_argument("--lang=ru-RU")
     opts.add_argument("--headless=new")
-    opts.add_argument("--no-sandbox")            # нужно в Docker
+    opts.add_argument("--no-sandbox") # нужно в Docker
     opts.add_argument("--disable-dev-shm-usage")
     opts.add_argument("--remote-allow-origins=*") 
     driver = webdriver.Chrome(options=opts)
