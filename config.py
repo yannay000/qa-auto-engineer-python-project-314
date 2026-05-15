d10ca4f4a5a5ce688134b549b70166ef90a14f51
Koval Victor <yan1992@yandex.ru>
2026-05-15 19:47:28 +0300

    add APP_BASE_URL to coverage run

--- config.py
+++ config.py
@@ -3,5 +3,7 @@ import os
 IMPLEMENTATION = os.getenv("IMPLEMENTATION")
 if IMPLEMENTATION: 
 	os.environ['APP_BASE_URL'] = f"http://{IMPLEMENTATION}.test"
+else:
+	os.environ['APP_BASE_URL'] = os.getenv("APP_BASE_URL", "http://localhost:5173")
 LOGIN = os.getenv("LOGIN", "test")
 PASSWORD = os.getenv("PASSWORD", "test")
