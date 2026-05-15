82239624512f5635c3ad468679213a8c2a7a9b23
Koval Victor <yan1992@yandex.ru>
2026-05-15 18:33:26 +0300

    print IMPLEMENTATION

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 IMPLEMENTATION = os.getenv("IMPLEMENTATION")
+print(f"IMPLEMENTATION: {IMPLEMENTATION}")
 if IMPLEMENTATION: 
 	os.environ['APP_BASE_URL'] = f"http://{IMPLEMENTATION}.test" 
 LOGIN = os.getenv("LOGIN", "test")
